74dcf38f94d17c3dff015cc4d3173685a8e1d2da
asen <1532781819@qq.com>
2022-09-15 23:08:36 +0800

    ‘第一次提交’

---
+++ vue/19.单文件组件/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <School/>
+        <Sdutent/>
+    </div>
+</template>
+
+<script>
+// 引入组件
+import School from './School'
+import Sdutent from './Sdutent'
+
+export default {
+    name:'App',
+    components:{
+        School,
+        Sdutent
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ vue/19.单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- 组件的结构 -->
+  <div>
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地点：{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码（数据 方法等等）
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "邵阳职业",
+      address: "邵阳",
+    }
+  },
+  methods: {
+    showName() {
+      console.log(this.name, this);
+    },
+  },
+}
+</script>
+
+<style>
+/* 组件的样式 */
+</style>
